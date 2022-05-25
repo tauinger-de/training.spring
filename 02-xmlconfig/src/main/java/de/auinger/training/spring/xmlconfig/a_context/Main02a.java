06cb977fdacab45e7616b750234449f12566d8a5
Thomas Auinger <thomas@auinger.de>
2022-05-25 12:07:18 +0200

    first steps

--- 02-xmlconfig/src/main/java/de/auinger/training/spring/xmlconfig/a_context/Main02a.java
+++ 02-xmlconfig/src/main/java/de/auinger/training/spring/xmlconfig/a_context/Main02a.java
@@ -1,9 +1,13 @@
 package de.auinger.training.spring.xmlconfig.a_context;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main02a {
 
     public static void main(String[] args) {
-        // todo
+        var applicationContext = new ClassPathXmlApplicationContext("context.xml", Main02a.class);
+        var testString = applicationContext.getBean("test", String.class);
+        System.out.println(testString);
     }
 
 }
