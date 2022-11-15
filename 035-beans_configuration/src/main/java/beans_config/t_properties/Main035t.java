0d40ada344ca8f4ef40defa794fcb9a65498002c
Thomas Auinger <thomas@auinger.de>
2022-11-15 13:53:49 +0100

    extended 035t

--- 035-beans_configuration/src/main/java/beans_config/t_properties/Main035t.java
+++ 035-beans_configuration/src/main/java/beans_config/t_properties/Main035t.java
@@ -1,10 +1,25 @@
 package beans_config.t_properties;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+import java.net.URL;
+import java.util.Arrays;
+
+// !!!
+// Run this class with the following system args:
+// -Dtest-prop=Servus,Welcome,Ciao -Dapp.config.base-url=https://www.example.com
+// !!!
 @Configuration
 public class Main035t {
+
+    @Value("${test-prop}")
+    private String[] testValues;
+
+    @Value("${app.config.base-url}")
+    URL baseUrl;
 
     public static void main(String[] args) {
         // using try syntax performs auto-close on context, which again calls destroy-method of H2 server
@@ -13,5 +28,10 @@ public class Main035t {
         // get and use property instance
         var appProperties = ctx.getBean(AppProperties.class);
         System.out.println(appProperties.getGreeting());
+    }
+
+    @PostConstruct
+    public void printStuff() {
+        System.out.println(Arrays.toString(this.testValues));
     }
 }
