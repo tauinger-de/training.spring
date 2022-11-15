26abc477f8a641ab76b54a6597f4a6f594dda9aa
Thomas Auinger <thomas@auinger.de>
2022-11-15 09:58:05 +0100

    ctx.print

--- 030-annotation_context/src/main/java/annotation_context/b_beans_and_injections/Main030b.java
+++ 030-annotation_context/src/main/java/annotation_context/b_beans_and_injections/Main030b.java
@@ -1,6 +1,6 @@
 package annotation_context.b_beans_and_injections;
 
-import core.Fmt;
+import core.Ctx;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main030b {
@@ -10,10 +10,7 @@ public class Main030b {
         var applicationContext = new AnnotationConfigApplicationContext(Main030b.class.getPackageName());
 
         // write data
-        Fmt.printf("The context contains %d bean(s):", applicationContext.getBeanDefinitionCount());
-        for (String name : applicationContext.getBeanDefinitionNames()) {
-            Fmt.printf("  - %s", name);
-        }
+        Ctx.printDetails(applicationContext);
 
         // get Runnables -- those implement our tests
         applicationContext.getBeansOfType(Runnable.class).values()
