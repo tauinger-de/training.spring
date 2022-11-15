2daac8728247fc94861464df3e7062ce68f81988
Thomas Auinger <thomas@auinger.de>
2022-11-15 13:54:18 +0100

    comment, constr-injct

--- 035-beans_configuration/src/main/java/beans_config/t_properties/AppProperties.java
+++ 035-beans_configuration/src/main/java/beans_config/t_properties/AppProperties.java
@@ -1,16 +1,18 @@
 package beans_config.t_properties;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:/beans_config/t_properties/app.properties")
+@PropertySource("classpath:/beans_config/t_properties/app.properties") // must use fully-qualified (absolute) path
 public class AppProperties {
 
-    @Autowired
-    private Environment env;
+    private final Environment env;
+
+    public AppProperties(Environment env) {
+        this.env = env;
+    }
 
     public String getGreeting() {
         return env.getProperty("app.greeting");
