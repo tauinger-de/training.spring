c9cd05a7317b9e7ccbfb66a597ea410c5bca1687
Thomas Auinger <thomas@auinger.de>
2022-11-15 11:38:35 +0100

    extended exrc 035q

---
+++ 035-beans_configuration/src/main/java/beans_config/q_import/EXERCISES-035o.md
@@ -0,0 +1,9 @@
+# Übung
+
+Schauen Sie sich den Source-Code von diesem Package an und machen sich ein Bild, was hier genau erzeugt wird.
+
+Über welchen Weg kommt die Bean mit Namen "greeting" in den Context?
+
+Über welchen die `PowerOfCalculator` Instanz?
+
+Woher kommt die Bean mit Namen "setterInjectedMathService"?
--- 035-beans_configuration/src/main/java/beans_config/q_import/Main035q.java
+++ 035-beans_configuration/src/main/java/beans_config/q_import/Main035q.java
@@ -2,11 +2,12 @@ package beans_config.q_import;
 
 import beans_config.r_import2.AdditionalConfiguration;
 import core.Ctx;
-import core.Fmt;
 import core.math.calculator.PowerOfCalculator;
-import core.math.calculator.SumCalculator;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.*;
+import core.math.service.MathService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import(AdditionalConfiguration.class)
@@ -14,15 +15,19 @@ import org.springframework.context.annotation.*;
 public class Main035q {
 
     public static void main(String[] args) {
-        // create
+        // create and print details
         var ctx = new AnnotationConfigApplicationContext(Main035q.class.getPackageName());
+        Ctx.printDetails(ctx);
 
         // say hello
         System.out.println(
-            ctx.getBean("greeting", String.class)
+                ctx.getBean("greeting", String.class)
         );
 
-        //
+        // get a calculator
         ctx.getBean(PowerOfCalculator.class);
+
+        // get a math-service
+        ctx.getBean("setterInjectedMathService", MathService.class);
     }
 }
